cff7e1419b834684475899f04a6bb5c0d26f00aa
ostreae <khadasevitch@gmail.com>
2016-10-17 23:35:51 +0300

    дз добавлено

---
+++ hw3/v4.py
@@ -0,0 +1,11 @@
+a = int(input('a = '))
+b = int(input('b = '))
+c = int(input('c = '))
+if (a + b == c):
+    print('обладают свойством 1')
+else
+    print('не обладают свойством 1')
+if (a/b==c):
+    print('обладают свойством 5')
+else
+    print('не обладают свойством 5')
