160a9e25104cd24ac7f293af9f75d32bbb869be3
ostreae <khadasevitch@gmail.com>
2016-10-17 23:38:25 +0300

    изменения

--- hw3/v4.py
+++ hw3/v4.py
@@ -3,9 +3,9 @@ b = int(input('b = '))
 c = int(input('c = '))
 if (a + b == c):
     print('обладают свойством 1')
-else
+else:
     print('не обладают свойством 1')
 if (a/b==c):
     print('обладают свойством 5')
-else
+else:
     print('не обладают свойством 5')
