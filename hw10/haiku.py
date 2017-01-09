7f9e0a1cd6eca6631d08a79427bd0ff350c871a6
ostreae <khadasevitch@gmail.com>
2017-01-10 02:15:14 +0300

    хайку с txt-файлами

--- hw10/haiku.py
+++ hw10/haiku.py
@@ -1,16 +1,24 @@
 import random
 
 
+def makelist(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        for line in f.readlines():
+            words = line.split(',')
+            words[-1] = words[-1].strip('\n')
+        return words
+
+
 def punctuation():
-    marks = ['!', '.', '...']
+    marks = makelist('punctuation.txt')
     return random.choice(marks)
 
 
 def nouns(mean):
-    noun_s = ['la neige', 'la merde', 'le chat', 'le chien', 'la nuit', 'le doigt', 'le pied', 'la branche', 'la ville', 'le lit', 'la salle', 'le soir', 'le cul', 'le lait']
-    noun_pl = ['les cheveux', 'les châteaux', 'les genoux', 'les vitraux']
-    noun_fem = ['tête', 'femme', 'veuve', 'sotte', 'brune', 'mère', 'tante']
-    noun_geo = ['l\'Asie', 'l\'Afrique', 'l\'Europe', 'la France']
+    noun_s = makelist('noun_s.txt')
+    noun_pl = makelist('noun_pl.txt')
+    noun_fem = makelist('noun_fem.txt')
+    noun_geo = makelist('noun_geo.txt')
     if mean == 's':
         return random.choice(noun_s)
     elif mean == 'pl':
@@ -22,33 +30,34 @@ def nouns(mean):
 
 
 def verbs():
-    verb = ['quitte', 'donne', 'laisse', 'est', 'dit', 'paie', 'soigne']
+    verb = makelist('verbs.txt')
     return random.choice(verb)
 
 
 def adjectives():
-    adjective = ['français', 'orange', 'marron', 'rapids', 'idéaux', 'heureux']
+    adjective = makelist('adjectives.txt')
     return random.choice(adjective)
 
 
 def interjections():
-    interjection = ['ah', 'aie', 'crac', 'diable', 'boum', 'ouf']
+    interjection = makelist('interjections.txt')
     return random.choice(interjection)
 
 
 def prepositions():
-    preposition = ['sur', 'au', 'dans', 'chez', 'sous']
+    preposition = makelist('prepositions.txt')
     return random.choice(preposition)
 
 
 def indicators():
-    indicator = ['cette', 'ma', ]
+    indicator = makelist('indicators.txt')
     return random.choice(indicator)
 
 
 def verse1():
     return nouns('s') + ' ' + verbs() + ' ' + nouns('s') + punctuation()
 print(verse1())
+
 
 def verse2():
     return interjections() + punctuation() + ' ' + prepositions() + ' ' + indicators() + ' ' + nouns('fem') + ' est' + ' ' + nouns('geo')
@@ -58,6 +67,3 @@ print(verse2())
 def verse3():
     return nouns('pl') + ' ' + adjectives() + punctuation()
 print(verse3())
-
-
-
